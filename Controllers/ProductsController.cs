3948657ab5af40c60041b9875adb67bfb95bfee9
FelesFurva <kitten.solar@gmail.com>
2022-06-13 19:04:53 +0300

    -minor edits -id now is created properly for new entries

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -5,73 +5,90 @@ namespace Homework1.Controllers
 {
     public class ProductsController : Controller
     {
-        List<ProductsModel> productsModels;
-
-        public ProductsController()
-        {
-            productsModels = new List<ProductsModel>
+       public static List<ProductsModel> productsModels = new List<ProductsModel>
+       {
+            new ProductsModel
             {
-                new ProductsModel
-                {
-                    Id = 1,
-                    Name = "Banana",
-                    Description = "They are yellow",
-                    Price = 1.59,
-                    CategoryID = 2
-                },
-                new ProductsModel
-                {
-                    Id=2,
-                    Name = "Kiwi",
-                    Description = "They are green",
-                    Price = 0.59,
-                    CategoryID = 2
-                },
-                new ProductsModel
-                {
-                    Id=3,
-                    Name = "Cake",
-                    Description = "Shh! We know ;)",
-                    Price = 10.00,
-                    CategoryID = 2
-                },
-                new ProductsModel
-                {
-                    Id=4,
-                    Name = "Potatoe",
-                    Description = "Life!",
-                    Price = 0.49,
-                    CategoryID = 1
-                },
-                new ProductsModel
-                {
-                    Id=5,
-                    Name = "Paprika",
-                    Description = "It's red",
-                    Price = 1.50,
-                    CategoryID = 1
-                },
-                new ProductsModel
-                {
-                    Id=6,
-                    Name = "Pepsi",
-                    Description = "No sugar",
-                    Price = 0.79,
-                    CategoryID = 3
-                }
+                Id = 1,
+                Name = "Banana",
+                Description = "They are yellow",
+                Price = 1.59,
+                CategoryID = 2,
+                Location = "row 6"
+            },
+            new ProductsModel
+            {
+                Id=2,
+                Name = "Kiwi",
+                Description = "They are green",
+                Price = 0.59,
+                CategoryID = 2,
+                Location = "row 6"
 
+            },
+            new ProductsModel
+            {
+                Id=3,
+                Name = "Cake",
+                Description = "Shh! We know ;)",
+                Price = 10.00,
+                CategoryID = 2,
+                Location = "row 6"
+            },
+            new ProductsModel
+            {
+                Id=4,
+                Name = "Potatoe",
+                Description = "Life!",
+                Price = 0.49,
+                CategoryID = 1,
+                Location = "row 4"
+            },
+            new ProductsModel
+            {
+                Id=5,
+                Name = "Paprika",
+                Description = "It's red",
+                Price = 1.50,
+                CategoryID = 1,
+                Location = "row 3"
+            },
+            new ProductsModel
+            {
+                Id=6,
+                Name = "Pepsi",
+                Description = "No sugar",
+                Price = 0.79,
+                CategoryID = 3,
+                Location = "row 2"
+            }
+       };
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var product = new ProductsModel
+            {
+                Id = productsModels.Count + 1
             };
-         }
+            return View(product);
+        }
 
+        [HttpPost]
+        public IActionResult Create(ProductsModel product)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                productsModels.Add(product);
+                return RedirectToAction("Products");
+            }
+            return View(product);
+        }
 
         public IActionResult Products()
         {
             return View(productsModels);
-        }
-
-        public IActionResult Create()
-        {
-            return View();
         }
 
         public IActionResult FindById(int specific)
