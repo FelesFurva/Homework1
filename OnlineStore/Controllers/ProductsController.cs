e24a6b6e6c78e9d8bd698627034d195f98982bcb
FelesFurva <kitten.solar@gmail.com>
2022-06-26 13:06:46 +0300

    -adjusted product controller

--- OnlineStore/Controllers/ProductsController.cs
+++ OnlineStore/Controllers/ProductsController.cs
@@ -49,17 +49,36 @@ namespace OnlineStore.Controllers
 
         public IActionResult Products()
         {
-            return View(productManager.GetProducts());
+            var products = productManager.GetProducts();
+            var productList = products.Select(product => new ProductsModel {
+                ProductId = product.ProductId,
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price,
+                CategoryID = product.CategoryID,
+                Location = product.Location
+            });
+                        
+            return View(productList);
         }
 
         public IActionResult FindById(int specific)
         {
             var model = productManager.GetProductsByID(specific);
-            if(model == null)
+            var productModel = new ProductsModel
+            {
+                ProductId = model.ProductId,
+                Name = model.Name,
+                Description = model.Description,
+                Price = model.Price,
+                CategoryID = model.CategoryID,
+                Location = model.Location
+            };
+            if (productModel == null)
             {
                 return RedirectToAction("Products");
             }
-            return View(model);
+            return View(productModel);
         }
     }
 }
